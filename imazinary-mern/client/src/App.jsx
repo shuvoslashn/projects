6c648e7c48188209d68de97057ff10a016c1b51a
Shuvo Sarker <sarker3664@gmail.com>
2023-09-05 06:06:14 +0600

    Header complete

--- imazinary-mern/client/src/App.jsx
+++ imazinary-mern/client/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { Home, CreatePost } from './pages';
+import logo from './assets/logo.png';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    return (
+        <BrowserRouter>
+            {/* Header Start */}
+            <header className='w-full bg-white sm:px-8 px-4 py-4 shadow-2xl shadow-indigo-950/10'>
+                <div className='container  flex justify-between items-center'>
+                    <Link to={`/`}>
+                        <img src={logo} alt='logo' className='w-40 object-contain' />
+                    </Link>
+                    <Link to={`/create-post`} className='btn-1'>
+                        Create
+                    </Link>
+                </div>
+            </header>
+            {/* Header End */}
+        </BrowserRouter>
+    );
+};
 
-export default App
+export default App;
