a23ebbe69dc1dc993bb6db6c5d7ffeb0c485e22a
Shuvo Sarker <sarker3664@gmail.com>
2023-09-13 18:36:51 +0600

    property app initial setup done

---
+++ property-react-firebase/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1 className='text-8xl font-black opacity-10'>Property</h1>
+        </div>
+    );
+};
+
+export default App;
