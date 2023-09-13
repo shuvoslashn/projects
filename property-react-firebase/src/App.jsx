497de30370a42ca28c2d6b3f2ac5c895490119ab
Shuvo Sarker <sarker3664@gmail.com>
2023-09-13 18:51:11 +0600

    React Router Dom setup done

--- property-react-firebase/src/App.jsx
+++ property-react-firebase/src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/authComponents/Profile';
+import SignIn from './pages/authComponents/SignIn';
+import SignUp from './pages/authComponents/SignUp';
+import ForgotPassword from './pages/authComponents/ForgotPassword';
+import Offers from './pages/Offers';
 
 const App = () => {
     return (
-        <div>
-            <h1 className='text-8xl font-black opacity-10'>Property</h1>
-        </div>
+        <>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/profile' element={<Profile />} />
+                <Route path='/sign-in' element={<SignIn />} />
+                <Route path='/sign-up' element={<SignUp />} />
+                <Route path='/forgot-password' element={<ForgotPassword />} />
+                <Route path='/offers' element={<Offers />} />
+                <Route path='/' element={<Home />} />
+            </Routes>
+        </>
     );
 };
 
