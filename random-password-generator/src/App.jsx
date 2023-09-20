37875088ebfbe017cda377064efa85178cf536f4
Shuvo Sarker <sarker3664@gmail.com>
2023-09-20 17:13:26 +0600

    state defined for project

--- random-password-generator/src/App.jsx
+++ random-password-generator/src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { useState } from "react";
 
 const App = () => {
-  return (
-    <div>
-        <h1 className="text-zinc-200">Random Password Generator</h1>
-    </div>
-  )
-}
+    const [length, setLength] = useState(8);
+    const [numAllowed, setNumAllowed] = useState(false);
+    const [charAllowed, setCharAllowed] = useState(false);
+    const [password, setPassword] = useState("");
 
-export default App
+    return (
+        <div>
+            <h1 className='text-zinc-200 text-center pt-8 text-2xl md:text-5xl font-semibold'>
+                Random Password Generator
+            </h1>
+        </div>
+    );
+};
+
+export default App;
