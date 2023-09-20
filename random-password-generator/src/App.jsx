98254e9aa50985e48e81219c11debcc322d9e98d
Shuvo Sarker <sarker3664@gmail.com>
2023-09-20 17:33:49 +0600

    password generator function done

--- random-password-generator/src/App.jsx
+++ random-password-generator/src/App.jsx
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 const App = () => {
     const [length, setLength] = useState(8);
     const [numAllowed, setNumAllowed] = useState(false);
     const [charAllowed, setCharAllowed] = useState(false);
     const [password, setPassword] = useState("");
+
+    const passGenerator = useCallback(() => {
+        let pass = "";
+        let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        numAllowed & (str += "0123456789");
+        charAllowed & (str += "!@#$%^&*-_+=[]{}~`");
+
+        length.forEach((item) => {
+            let char = Math.floor(Math.random() * str.length + 1);
+            pass = str.charAt(char);
+        });
+
+        setPassword(pass);
+        
+    }, [length, numAllowed, charAllowed, setPassword]);
 
     return (
         <div>
