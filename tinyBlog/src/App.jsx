47ef6dfd1ffa67cd2eaad1b40b0f6b13ca4a3ab6
Shuvo Sarker <sarker3664@gmail.com>
2023-10-15 20:56:11 +0600

    basic setup

---
+++ tinyBlog/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div className='w-full h-screen flex justify-center items-center'>
+            <h2 className='text-8xl'>Simple Blog</h2>
+        </div>
+    );
+};
+
+export default App;
