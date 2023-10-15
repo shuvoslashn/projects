2eaad25058090e6bb73a0dab73e7e3f73a4899b6
Shuvo Sarker <sarker3664@gmail.com>
2023-10-15 14:00:06 +0600

    footer done

--- portfolioV2/src/components/Footer.jsx
+++ portfolioV2/src/components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <section className='py-8 bg-[#E3E3FF]'>
+            <div className='container text-center'>
+                <p className='text-xl'>
+                    &copy; All rights reserved by Shuvo Sarker
+                </p>
+            </div>
+        </section>
+    );
 };
 
 export default Footer;
